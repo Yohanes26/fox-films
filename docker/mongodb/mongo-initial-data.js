a0f289eb3ca2642b6cab8876f214b7a00bbfbdcb
Yohanes Lopes <yohanes.lopes@empiricus.com.br>
2019-05-24 19:50:04 -0300

    Correcao no host do mongo e inclusao de create user chamado admin na database do common para ter acesso readWrite

--- docker/mongodb/mongo-initial-data.js
+++ docker/mongodb/mongo-initial-data.js
@@ -1,3 +1,17 @@
 db.directors.insertMany([
     {name: "Pé de pano", birthdayDate: "1900-10-11", films: [{name: "Go Horse", year: "2000"}]}
 ]);
+
+
+db.createUser(
+    {
+        user: "admin",
+        pwd: "123456",
+        roles:[
+            {
+                role: "readWrite",
+                db:   "common"
+            }
+        ]
+    }
+);
